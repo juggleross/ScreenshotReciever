a8f452c252ccbdf8c6f864a6603f436dd50be374
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-09-03 15:41:19 +0300

    Change sqlite to pg

--- app/helpers/screenshots_helper.rb
+++ app/helpers/screenshots_helper.rb
@@ -1,2 +1,8 @@
 module ScreenshotsHelper
+  def message_for_empty_gallery(screenshots)
+    if screenshots.empty?
+      data = "<h1>There is no any screenshots</h1>"
+      data.html_safe
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180830141533) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "screenshots", force: :cascade do |t|
     t.string "photo"
     t.string "name"
