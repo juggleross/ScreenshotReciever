8829d7edbbd459a71f21fc023f0ac11151ae34da
Vladislav Hilko <vladislav.spawn@gmail.com>
2018-09-03 15:05:21 +0300

    Remove unnecessary scaffold actions

--- app/views/screenshots/_screenshot.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! screenshot, :id, :photo, :name, :created_at, :updated_at
-json.url screenshot_url(screenshot, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'screenshots#index'
-  resources :screenshots
+  resources :screenshots, only: [:index]
 
   post 'add_screenshot', controller: 'screenshots', action: 'add_screenshot'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
